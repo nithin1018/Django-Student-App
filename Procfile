f1da6d3d88c1b81d7bd839425b4d9d69d26f489b
Nithin Jayakumar <nithinjayakumar2205@gmail.com>
2025-06-10 11:16:22 +0530

    Added procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn day1.wsgi
--- day1/settings.py
+++ day1/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
